a1e2d7169a88cae16ac979ff1f9ad0b40ed797fb
Michael Ekwueme <ekwuemem@uci.edu>
2024-11-17 10:08:30 -0800

    connected the mongodb database and now working on cladinary

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 
+// connecting to the database
 const connectDB = async () => {
-    await mongoose.connect(`${process.env.MONGODB_URI}`)
+    mongoose.connection.on("connected", () =>{
+        console.log("DB Connected");
+    })
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
 }
 
 export default connectDB;
