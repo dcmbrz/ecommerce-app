dbcffcfb5b9382c4668f9687b56a0147f9cd341f
dcmbrz <ekwuemem@uci.edu>
2025-02-06 18:27:37 -0800

    getting an error

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,6 @@
 // *** Controller function to add products ***
 
+import {v2 as cloudinary} from 'cloudinary'
 
 // funciton for adding a product
 const addProduct = async (req, res) => {
@@ -12,8 +13,17 @@ const addProduct = async (req, res) => {
         const image3 = req.files.image3 && req.files.image3[0]
         const image4 = req.files.image4 && req.files.image4[0]
 
+        const images = [image1, image2, image3, image4].filter((item)=> item !== undefined)
+
+        let imageUrl = await Promise.all(
+            images.map(async (item) =>{
+                let result = await cloudinary.uploader.upload(item.path,{resource_type: "image"})
+                return result.secure_url
+            })
+        )
+
         console.log(name, description, price, category, subCategory, sizes, bestseller)
-        console.log(image1, image2, image3, image4)
+        console.log(imagesUrl)
 
         res.json({})
 
