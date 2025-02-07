40b04a96a9940dd9531f6f9de39410abefc0d5a7
ashv2ley <ashley0425@yahoo.com>
2025-02-07 14:23:37 -0800

    made changes to frontend and imported shadcn components

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { Search, AlignJustify, Heart, ShoppingCart} from "lucide-react";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import ProfileDropdown from "./ProfileDropdown.jsx";
+
 function Navbar() {
+    const location = useLocation();
+    const hideNavbar = location.pathname === '/sign-in' || location.pathname === '/sign-up';
+
+    if (hideNavbar) {
+        return null; // Hide navbar on sign-in or sign-up pages
+    }
     return (
         <div className={"flex justify-between items-center px-5 py-3 border-b"}>
 
@@ -24,11 +31,11 @@ function Navbar() {
             </div>
 
             <div className={"flex gap-5 font-medium text-lg items-center justify-center"}>
+                <Link to={"/sign-in"} className={"relative px-4 py-2 ease-in-out duration-300 hover:bg-gray hover:px-4 hover:py-2 hover:rounded-sm"}>Sign in</Link>
                 <Link to={"/favorites"}><Heart/></Link>
                 <Link to={"/cart"}><ShoppingCart/></Link>
                 <ProfileDropdown/>
                 {/*<Link to={"/sign-up"} className={"border-2 p-1 px-5 rounded-lg border-blue-500"}>Sign up</Link>*/}
-                {/*<Link to={"/sign-in"}>Sign in</Link>*/}
             </div>
 
 
